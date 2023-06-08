4c1296642d9bb5c0e28719746cf4bc80f29a26fa
Juan Sebastian Torres Rodriguez <126811520+JSebastianTorresR@users.noreply.github.com>
2023-06-07 22:48:29 -0500

    Add files via upload

--- ESP32/main.py
+++ ESP32/main.py
@@ -3,43 +3,44 @@
 # taller de electronica unal 2023-1
 
 #Modulos
-import usocket as socket
+import usocket
 import mpu6050
 import time
 import math
 from machine import Pin, I2C
+import network
 
-# Agregar ip y port de donde está el servidor UDP
-serverAddressPort = ("192.168.29.225", 3001)
-# Cantidad de bytes a recibir
-bufferSize  = 128
 
-# Create a UDP socket at client side
-sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#conectar wifi
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
 
-# Función para enviar datos
+def connectWifi(ssid, pwd):
+    while not wlan.isconnected():
+        print('connecting to network...')
+        time.sleep(1)
+        if wlan.status() == 1001:
+            pass
+        else:
+            wlan.connect(ssid, pwd)
+            
+if not wlan.isconnected():
+    connectWifi("MagicGlove","aeiouaei")
+else:
+    print('network config:', wlan.ifconfig())
+    
+# Crear socket UDP
+server = usocket.socket(usocket.AF_INET, usocket.SOCK_DGRAM)
+serverAdressPort = ("192.168.12.8", 12345)
+#server.connect(serverAdressPort)
+
 def sendUDP(data):
     try:
-        sk.sendto(str.encode(data), serverAddressPort)
+        server.sendto(str.encode(data), serverAdressPort)
     except:
         print("error send")
-
-# Función para recibir datos
-def receiveUDP():
-    msg = sk.recvfrom(bufferSize)
-    return "{}".format(msg[0])
- 
-# Activar si el esp32 será una estación
-from wifiSTA import connectSTA as connect
-
-# Activar si el esp32 estará en modo de acceso AP
-
-#from wifiAP import apConfig as connect
-# poner acá el nombre de red ssid y password para conectarse
-connect("Esp32", "aeiouaei")
-
-#para usar giroscopio
-
+        
+# Usar Giroscopio
 i2c = I2C(scl = Pin(22), sda = Pin(21))
 mpu = mpu6050.accel(i2c)
 
@@ -60,8 +61,32 @@ while True:
     print("grados z: ", gradosZ)
     print("temperatura: ", tmp)
     print()
-    sleep_ms(50)
-    sendUDP(str(gradosX))
-    #print(receiveUDP())
+    time.sleep_ms(10)
+    sendUDP(str(gradosY))
+
+# Agregar ip y port de donde está el servidor UDP
+#serverAddressPort = ("192.168.17.8", 3001)
+# Cantidad de bytes a recibir
+#bufferSize  = 128
+
+# Create a UDP socket at client side
+#sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Función para enviar datos
+
+# Función para recibir datos
+#def receiveUDP():
+ #   msg = sk.recvfrom(bufferSize)
+  #  return "{}".format(msg[0])
+ 
+# Activar si el esp32 será una estación
+#from wifiSTA import connectSTA as connect
+# Activar si el esp32 estará en modo de acceso AP
+#from wifiAP import apConfig as connect
+# poner acá el nombre de red ssid y password para conectarse
+#connect("MagicGlove", "aeiouaei")
+
+
+
     
 
